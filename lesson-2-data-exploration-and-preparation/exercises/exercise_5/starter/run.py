6edc1faaeaf9101c5b9a967308b54a8f9662e61f
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-09 15:18:36 +0700

    add cleansing script to run.py

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -3,7 +3,7 @@ import argparse
 import logging
 import pandas as pd
 import wandb
-
+import os
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -14,7 +14,31 @@ def go(args):
     run = wandb.init(project="exercise_5", job_type="process_data")
 
     ## YOUR CODE HERE
-    pass
+    logger.info('Fetching artifact')
+    artifact = run.use_artifact(f'{args.input_artifact}')
+    local_path = artifact.file()
+
+    logger.info('Reading dataframe')
+    df = pd.read_parquet(local_path)
+
+    # Pre-processing
+    logger.info('Start pre-processing')
+    df = df.drop_duplicates().reset_index(drop=True)
+    df['title'].fillna('', inplace=True)
+    df['song_name'].fillna('', inplace=True)
+    df['text_feature'] = f"{df['title']} {df['song_name']}"
+
+    # Save data and upload artifact
+    output_file = args.artifact_name
+    df.to_csv(output_file)
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description
+    )
+    artifact.add_file(output_file)
+    run.log_artifact(artifact)
+    os.remove(output_file)
 
 
 if __name__ == "__main__":
